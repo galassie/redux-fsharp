180498bebdb7402202a953d373c91ce44dd4cd37
Enrico Galassi <enrico.galassi.88@gmail.com>
2019-11-20 23:59:56 +0100

    Add tests for subscribe and unsubscribe on Store

--- tests/Redux.FSharp.Tests/StoreTests.fs
+++ tests/Redux.FSharp.Tests/StoreTests.fs
@@ -1,6 +1,7 @@
 module Redux.FSharp.Tests
 
 open NUnit.Framework
+open Redux.Types
 open Redux.Store
 
 [<Test>]
@@ -17,7 +18,30 @@ let StoreGetStateShouldReturnProperState () =
     let mockReducer state action =
         match action with
         | "increment" -> state + 1
-        | "decrement" -> state + 1
+        | "decrement" -> state - 1
+        | _ -> state
+    let initialState = 0
+
+    let store = createStore mockReducer initialState
+
+    store.Dispatch "increment" |> ignore
+    store.Dispatch "decrement" |> ignore
+    store.Dispatch "increment" |> ignore
+    store.Dispatch "increment" |> ignore
+
+    Assert.AreEqual(2, store.GetState())
+
+[<Test>]
+let StoreDispatchShouldNotifyAllSubscribers () =
+    let mutable subscriberState1 = 0
+    let mutable subscriberState2 = 5
+    let subscriber1 = { new IStoreSubscriber<int> with member this.OnNewState state = subscriberState1 <- state }
+    let subscriber2 = { new IStoreSubscriber<int> with member this.OnNewState state = subscriberState2 <- state }
+    
+    let mockReducer state action =
+        match action with
+        | "increment" -> state + 1
+        | "decrement" -> state - 1
         | _ -> state
     let initialState = 0
 
@@ -25,6 +49,51 @@ let StoreGetStateShouldReturnProperState () =
 
     store.Dispatch "increment" |> ignore
     store.Dispatch "increment" |> ignore
+
+    store.Subscribe(subscriber1) |> ignore
+    store.Subscribe(subscriber2) |> ignore
+
+    store.Dispatch "increment" |> ignore
+    store.Dispatch "increment" |> ignore
+    store.Dispatch "decrement" |> ignore
+
+    Assert.AreEqual(3, subscriberState1)
+    Assert.AreEqual(3, subscriberState2)
+
+
+[<Test>]
+let StoreUnsubscribeShouldProperlyUnsubscribeSubscribers () =
+    let mutable subscriberState1 = 0
+    let mutable subscriberState2 = 5
+    let subscriber1 = { new IStoreSubscriber<int> with member this.OnNewState state = subscriberState1 <- state }
+    let subscriber2 = { new IStoreSubscriber<int> with member this.OnNewState state = subscriberState2 <- state }
+    
+    let mockReducer state action =
+        match action with
+        | "increment" -> state + 1
+        | "decrement" -> state - 1
+        | _ -> state
+    let initialState = 0
+
+    let store = createStore mockReducer initialState
+
+    store.Dispatch "increment" |> ignore
     store.Dispatch "increment" |> ignore
 
-    Assert.AreEqual(3, store.GetState())
+    let unsubscribe1 = store.Subscribe(subscriber1)
+    let unsubscribe2 = store.Subscribe(subscriber2)
+
+    store.Dispatch "decrement" |> ignore
+
+    let resultUnsubscribe1 = unsubscribe1()
+
+    store.Dispatch "increment" |> ignore
+
+    let resultUnsubscribe2 = unsubscribe2()
+
+    store.Dispatch "increment" |> ignore
+
+    Assert.IsTrue(resultUnsubscribe1)
+    Assert.IsTrue(resultUnsubscribe2)
+    Assert.AreEqual(1, subscriberState1)
+    Assert.AreEqual(2, subscriberState2)
